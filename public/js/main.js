eb1e69951b39a6ed5e10bb4a1884a821a4fb4b24
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2020-11-11 09:53:53 -0700

    Display Kanto Pokemon when any button is clicked

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,70 @@
 const kantoButton = document.querySelector('[data-kanto-button]')
+const johtoButton = document.querySelector('[data-johto-button]')
+const hoennButton = document.querySelector('[data-hoenn-button]')
 
-let fetchKantoPokemon = fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-    .then(response => response.json());
-fetchKantoPokemon
-    .then(allpokemon => console.log(allpokemon));
 
-kantoButton.addEventListener('click', () => {
-    document.getElementById('pageContent').innerHTML = kantoPokemonHtml;
+function fetchPokemonData(pokemon) {
+    let url = pokemon.url;
+    fetch(url)
+        .then(response => response.json())
+        .then(pokeData => {
+            renderPokemon(pokeData)
+        })
+}
+
+
+function renderPokemon(pokeData) {
+    const allPokemonContainer = document.getElementById('poke-container');
+    const pokeContainer = document.createElement('div');
+    const pokeName = document.createElement('h4');
+    const pokeNumber = document.createElement('p');
+
+    pokeName.innerText = pokeData.name;
+    pokeNumber.innerText = `#${pokeData.id}`;
+
+    pokeContainer.append(pokeName, pokeNumber);
+    allPokemonContainer.appendChild(pokeContainer);
+}
+
+function renderAllPokemon() {
+    const allPokemonContainer = document.getElementById('poke-container');
+    allPokemonContainer.innerText = '';
+    fetchKantoPokemon();
+}
+
+
+function fetchKantoPokemon() {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+        .then(response => response.json())
+        .then(allpokemon => {
+            allpokemon.results.forEach(pokemon => {
+                fetchPokemonData(pokemon);
+            });
+        })
+}
+function fetchJohtoPokemon() {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=151')
+        .then(response => response.json())
+        .then(allpokemon => {
+            allpokemon.results.forEach(pokemon => {
+                fetchPokemonData(pokemon);
+            });
+        })
+}
+function fetchHoennPokemon() {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=135&offset=251')
+        .then(response => response.json())
+        .then(allpokemon => {
+            allpokemon.results.forEach(pokemon => {
+                fetchPokemonData(pokemon);
+            });
+        })
+}
+
+kantoButton.addEventListener('click', renderAllPokemon)
+johtoButton.addEventListener('click', renderAllPokemon);
+hoennButton.addEventListener('click', () => {
+    document.getElementById('pageContent').innerHTML = hoennPokemonHtml;
+    fetchHoennPokemon
+        .then(allpokemon => document.getElementById('regionPokemon').innerHTML = allpokemon)
 })
--- public/js/templates.js
+++
@@ -1,4 +0,0 @@
-let kantoPokemonHtml = `
-    <h2>Kanto Pokemon!</h2>
-    <div></div>
-`;
