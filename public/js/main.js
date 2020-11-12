7e276c477b806b6f4f801b4cb31f414faef535dc
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2020-11-11 19:38:33 -0700

    Add styling and functionality

--- public/js/main.js
+++ public/js/main.js
@@ -13,11 +13,15 @@ function fetchPokemonData(pokemon) {
 }
 
 
+
 function renderPokemon(pokeData) {
     const allPokemonContainer = document.getElementById('poke-container');
     const pokeContainer = document.createElement('div');
     const pokeName = document.createElement('h4');
     const pokeNumber = document.createElement('p');
+    pokeContainer.classList.add('card', 'col-sm-12', 'col-md-6', 'col-lg-4');
+
+    createPokeImage(pokeData.id, pokeContainer);
 
     pokeName.innerText = pokeData.name;
     pokeNumber.innerText = `#${pokeData.id}`;
@@ -26,12 +30,40 @@ function renderPokemon(pokeData) {
     allPokemonContainer.appendChild(pokeContainer);
 }
 
-function renderAllPokemon() {
+function createPokeImage(pokeID, containerDiv) {
+    const pokeImgContainer = document.createElement('div');
+    const pokeImage = document.createElement('img');
+
+    pokeImgContainer.classList.add('poke-image')
+    pokeImage.srcset = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`
+
+    pokeImgContainer.append(pokeImage);
+    containerDiv.append(pokeImgContainer);
+}
+
+
+function selectRegion(region) {
+    document.getElementById(region).style.backgroundColor = 'black';
+    document.getElementById(region).style.color = 'white';
+}
+
+
+
+function renderKantoPokemon() {
     const allPokemonContainer = document.getElementById('poke-container');
     allPokemonContainer.innerText = '';
     fetchKantoPokemon();
 }
-
+function renderJohtoPokemon() {
+    const allPokemonContainer = document.getElementById('poke-container');
+    allPokemonContainer.innerText = '';
+    fetchJohtoPokemon();
+}
+function renderHoennPokemon() {
+    const allPokemonContainer = document.getElementById('poke-container');
+    allPokemonContainer.innerText = '';
+    fetchHoennPokemon();
+}
 
 function fetchKantoPokemon() {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
@@ -61,10 +93,6 @@ function fetchHoennPokemon() {
         })
 }
 
-kantoButton.addEventListener('click', renderAllPokemon)
-johtoButton.addEventListener('click', renderAllPokemon);
-hoennButton.addEventListener('click', () => {
-    document.getElementById('pageContent').innerHTML = hoennPokemonHtml;
-    fetchHoennPokemon
-        .then(allpokemon => document.getElementById('regionPokemon').innerHTML = allpokemon)
-})
+kantoButton.addEventListener('click', renderKantoPokemon);
+johtoButton.addEventListener('click', renderJohtoPokemon);
+hoennButton.addEventListener('click', renderHoennPokemon);
